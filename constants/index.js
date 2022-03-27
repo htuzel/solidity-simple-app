f4b66318afd996e0b26cb65f331de759f6fa1c0e
TUZELH <TUZELH01@currys.co.uk>
2022-03-27 14:10:26 +0200

    first commit

---
+++ constants/index.js
@@ -0,0 +1,9 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x9D37E8E95C1462eE4Ef5ffb9511d4E047003A4d6";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://web3-whitelisting-92snh2a6k-htuzel.vercel.app/api/";
+
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xBE356088792B62eD6080Fe20a4c81D7c5a8F8572";
+
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL, CRYPTO_DEVS_NFT_CONTRACT_ADDRESS};
